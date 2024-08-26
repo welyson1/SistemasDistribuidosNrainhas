3b91ad9ecbf5e3e57517493b482434dadd02bb35
gabrielzj <gabrielzuinj@gmail.com>
2024-08-25 22:28:10 -0300

    refactor: N rainhas em arquivo separado, add paralelização

---
+++ Gabriel-threads/main.py
@@ -0,0 +1,45 @@
+import sequencial
+import paralelo
+
+"""
+    Máquina utilizada na execução:
+    
+    Processador: 
+    AMD Ryzen 7 4800H (8 cores, 16 threads), 2.9GHz - 4.2GHz, 16MB cache, 45W TDP
+    
+    Memória: 16Gb RAM
+    Armazenamento: SSD 1TB
+    
+    Versão do Python: 3.12.1
+    
+"""
+
+
+def main():
+    number_of_queens = [4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+    for n in number_of_queens:
+
+        all_solutions_sequencial, sequencial_time = sequencial.sequencial_solve(n)
+        all_solutions_paralelo, paralelo_time = paralelo.paralelo_solver(n)
+
+        total_solutions_sequencial = len(all_solutions_sequencial)
+        total_solutions_paralelo = len(all_solutions_paralelo)
+
+        sequencial_solutions_to_print = min(total_solutions_sequencial, 5)
+        paralelo_solutions_to_print = min(total_solutions_paralelo, 5)
+
+        print(f"Solução para {n}-Queen:\n")
+        print(f"Sequencial: {total_solutions_sequencial}\n")
+        for i, solution in enumerate(all_solutions_sequencial[:sequencial_solutions_to_print], 1):
+            print(f"{i}: {solution}")
+        print(f"Tempo: {sequencial_time:.5f} segundos\n")
+
+        print(f"Paralelo: {total_solutions_paralelo}\n")
+        for i, solution in enumerate(all_solutions_paralelo[:paralelo_solutions_to_print], 1):
+            print(f"{i}: {solution}")
+        print(f"Tempo: {paralelo_time:.5f} segundos\n")
+
+
+if __name__ == "__main__":
+    main()
--- Gabriel-threads/sequencial.py
+++ Gabriel-threads/sequencial.py
@@ -1,126 +1,8 @@
-"""
-    Pode-se representar o tabuleiro com 1 dimensão, reduzindo complexidade
-    Ex 1:
-    -----------------
-    |   | Q |   |   |
-    |   |   |   | Q |
-    | Q |   |   |   |
-    |   |   | Q |   |
-    -----------------
-
-    vetor que representa o tabuleiro [1, 3, 0, 2]
-
-    Ex 2:
-    -----------------
-    |   |   | Q |   |
-    | Q |   |   |   |
-    |   |   |   | Q |
-    |   | Q |   |   |
-    -----------------
-
-    vetor que representa o tabuleiro [2, 0, 3, 1]
-
-    O problema pode ser resolvido pela pela propriedade das diagonais:
-
-    A soma das coordenadas da diagonal da esquerda para a direita sempre são iguais a soma das coordenadas da rainha
-
-    A soma das coordenadas da diagonal da direita para a esquerda sempre são iguai a subtração das coordenadas da rainha
-
-    Ex: Diagonal esquerda para a direita
-
-        -----------------
-    0   |   |   |   |   |
-    1   |   |   | X |   |   =>> Rainha (2,1) = 2 + 1 = 3
-    2   |   | Q |   |   |       Diagonal superior direita => (1,2) = 1 + 2 = 3
-    3   | X |   |   |   |       Diagonal inferior esquerda => (3,0) = 3 + 0 = 3
-        -----------------
-          0   1   2   3
-
-        -----------------
-    0   |   |   |   |   |
-    1   | X |   |   |   |   =>> Rainha (2,1) = 2 - 1 = 1
-    2   |   | Q |   |   |       Diagonal superior esquerda => (1,0) = 1 - 0 = 1
-    3   |   |   | X |   |       Diagonal inferior direita => (3,2) = 3 - 2 = 1
-        -----------------
-          0   1   2   3
-"""
 import time
+from n_queens_solver import solve_n_queens
 
 
-def solve_n_queens(n):
-
-    """
-        Cada índice representa uma linha, o valor representa a coluna da rainha
-        Rainha na coluna 2 e linha 0 -> board[0] = 2
-        Ex: n = 4 -> board = [ * , * , * , *]
-    """
-
-    board = ['*'] * n
-
-    """
-        columns = [] -> Lista de colunas ocupadas
-        
-        positive_diagonals = []  -> Lista de diagonais positivas ocupadas (linha + coluna)
-        
-        negative_diagonals = []  -> Conjunto de diagonais negativas ocupadas (linha - coluna)
-        
-        solutions = []  -> Lista para armazenar todas as soluções
-    """
-
-    columns = []
-    positive_diagonals = []
-    negative_diagonals = []
-    solutions = []
-
-    # Uma posição é segura se sua coluna e diagonais não estão ocupadas
-    def valid_position(row, col):
-        return col not in columns and row + col not in positive_diagonals and row - col not in negative_diagonals
-
-    def place_queen(row):
-        # Verifica se todas as rainhas foram posicionadas
-        if row == n:
-            # Se sim, adiciona a solução atual à lista de soluções
-            solutions.append(board[:])
-            return
-        # Tenta colocar uma rainha em cada coluna da linha atual
-        for col in range(n):
-            # Verifica se a posição atual é segura
-            if valid_position(row, col):
-                # Posiciona a rainha
-                board[row] = col
-
-                # Marca a coluna e as diagonais como ocupadas
-                columns.append(col)
-
-                # Marca a diagonal positiva como ocupada
-                positive_diagonals.append(row + col)
-
-                # Marca a diagonal negativa como ocupada
-                negative_diagonals.append(row - col)
-
-                # Move para a próxima linha
-                place_queen(row + 1)
-
-                # Remove a marcação da coluna
-                columns.pop()
-
-                # Remove a marcação da diagonal positiva
-                positive_diagonals.pop()
-
-                # Remove a marcação da diagonal negativa
-                negative_diagonals.pop()
-
-    # Inicia o processo recursivo a partir da primeira linha
-    place_queen(0)
-    return solutions
-
-
-# Lista de tamanhos de tabuleiro para resolver
-number_of_queens = [4, 5, 6, 7, 8, 9, 10, 11, 12]
-
-# Resolve para cada tamanho de tabuleiro
-for n in number_of_queens:
-    print(f"\nResolvendo {n}-Rainhas:")
+def sequencial_solve(number_of_queens):
 
     """
         start_time -> inicia a contagem do tempo
@@ -134,22 +16,9 @@ for n in number_of_queens:
         de execução como o time() e o process_time()
     """
 
-    start_time = time.perf_counter()
-    all_solutions = solve_n_queens(n)
-    end_time = time.perf_counter()
+    start_time_sequencial = time.perf_counter()
+    all_solutions_sequencial = solve_n_queens(number_of_queens)
+    end_time_sequencial = time.perf_counter()
+    exec_time_sequencial = end_time_sequencial - start_time_sequencial
 
-    # Receve o tamanho da lista de soluções
-    total_solutions = len(all_solutions)
-
-    # Intevalo de tempo entre o início do registro e final
-    exec_time = end_time - start_time
-
-    print(f"Número de soluções: {total_solutions}")
-    print(f"Tempo de execução: {exec_time:.5f} segundos")
-
-    # Imprime até 10 soluções, se o total de soluções for maior que 10
-    solutions_to_print = min(total_solutions, 10)
-
-    # Imprime a solução
-    for i, solution in enumerate(all_solutions[:solutions_to_print], 1):
-        print(f"Solução {i}: {solution}")
+    return all_solutions_sequencial, exec_time_sequencial
